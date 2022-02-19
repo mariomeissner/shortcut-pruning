36567fc118fd041b1a5bb8fe05df2f824dbf24e4
Johannes Mario Meissner <jmariomeissner@gmail.com>
2022-02-19 12:33:51 +0900

    Add HANS eval callback

--- src/callbacks/hans_evaluation.py
+++ src/callbacks/hans_evaluation.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 from src.datamodules.hf_datamodule import HFDataModule
 from src.utils.utils import get_logger
+from pytorch_lightning.utilities import rank_zero_only
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
+from torchmetrics.classification.accuracy import Accuracy
 
 log = get_logger(__name__)
 
 
 class HansEvaluation(ModelCheckpoint):
-    def __init__(self, split_level: str, *args, **kwargs):
+    def __init__(self, split_level: str, tokenizer_name: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         if not split_level in ["global", "label", "lexical_types"]:
@@ -17,9 +19,28 @@ class HansEvaluation(ModelCheckpoint):
             raise NotImplementedError
 
         self.split_level = split_level
+        self.hans_datamodule = HFDataModule(
+            dataset_name="hans",
+            subdataset_name=None,
+            num_labels=2,
+            tokenizer_name=tokenizer_name,
+            sentence_1_name="premise",
+            sentence_2_name="hypothesis",
+        )
+        log.info("Setting up HANS callback.")
+        self.hans_datamodule.setup()
 
-        hans_datamodule = HFDataModule(dataset_name="hans")
-
-    def on_validation_end(self, trainer, pl_module):
-        pass
-        
+    @rank_zero_only
+    def on_validation_epoch_end(self, trainer, pl_module):
+        # TODO: Split by label when requested
+        log.info("Validating on HANS...")
+        hans_val_dataloader = self.hans_datamodule.val_dataloader()
+        # labels = self.hans_datamodule.dataset["validation"]["labels"]
+        device = pl_module.device
+        accuracy = Accuracy().to(device)
+        for batch in hans_val_dataloader:
+            batch = {k: v.to(device) for k, v in batch.items()}
+            labels = batch['labels']
+            preds = pl_module(batch)[1] # Get preds
+            accuracy(preds, labels)
+        pl_module.log("hans/val", accuracy.compute(), on_epoch=True)
