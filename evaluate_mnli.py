0b8ee7004ca5bc12d27d073530c94969f81dff6e
mariomeissner <jmariomeissner@gmail.com>
2022-06-16 21:49:49 +0900

    Update

--- evaluate_mnli.py
+++ evaluate_mnli.py
@@ -21,6 +21,7 @@ from src.models.hf_model_pruned import PruningTransformer
 def evaluate_on_mnli(
     checkpoint_path: str = None,
     checkpoint_list_file: str = None,
+    paste: str = None,
     pruned_model: bool = False,
     do_matched: bool = True,
     do_mismatched: bool = False,
@@ -37,77 +38,83 @@ def evaluate_on_mnli(
         with open(checkpoint_list_file) as _file:
             path_list = _file.readlines()
             path_list = [line.strip() + "/checkpoints/last.ckpt" for line in path_list]
+    elif paste:
+        path_list = [line.strip() + "/checkpoints/last.ckpt" for line in paste.split("\n")]
     else:
         raise ValueError("Provide either of checkpoint_path or checkpoint_list_file!")
 
-    for path in path_list:
+    if pruned_model:
+        print("Loading PruningTransformer instead of the normal one.", file=sys.stderr)
+        model_class = PruningTransformer
+    else:
+        model_class = SequenceClassificationTransformer
 
-        if pruned_model:
-            print("Loading PruningTransformer instead of the normal one.", file=sys.stderr)
-            model_class = PruningTransformer
-        else:
-            model_class = SequenceClassificationTransformer
+    model = model_class.load_from_checkpoint(path_list[0], use_bias_probs=False)
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    tokenizer = AutoTokenizer.from_pretrained(model.hparams.huggingface_model)
+
+    # Load dataset
+    mnli: DatasetDict = datasets.load_dataset("glue", "mnli")
+    # Remove unnecessary splits for speed
+    mnli = DatasetDict(
+        {"validation_matched": mnli["validation_matched"], "validation_mismatched": mnli["validation_mismatched"]}
+    )
+
+    def preprocess_func(examples: dict):
+        args = (examples["premise"], examples["hypothesis"])
+        result = tokenizer(*args, max_length=max_length, truncation=True)
+        return result
+
+    mnli = mnli.map(preprocess_func, batched=True, num_proc=4)
+    mnli = mnli.rename_column("label", "labels")
+    mnli.set_format(
+        "torch",
+        columns=[
+            "input_ids",
+            "attention_mask",
+            "token_type_ids",
+            "labels",
+            "idx",
+        ],
+    )
+    # train_dataloader = DataLoader(
+    #     mnli["train"], batch_size=batch_size, shuffle=False, collate_fn=DataCollatorWithPadding(tokenizer=tokenizer)
+    # )
+    matched_dataloader = DataLoader(
+        mnli["validation_matched"],
+        batch_size=batch_size,
+        shuffle=False,
+        collate_fn=DataCollatorWithPadding(tokenizer=tokenizer),
+    )
+    mismatched_dataloader = DataLoader(
+        mnli["validation_mismatched"],
+        batch_size=batch_size,
+        shuffle=False,
+        collate_fn=DataCollatorWithPadding(tokenizer=tokenizer),
+    )
+
+    with open("data/subsets/mnli_negation_indices.json") as _file:
+        negation_indices = json.load(_file)
+    with open("data/subsets/mnli_lex_overlap_indices.json") as _file:
+        overlap_indices = json.load(_file)
+    with open("data/subsets/mnli_neg_ent_new2.json") as _file:
+        new_neg_ent_indices = json.load(_file)
+
+    def evaluate_subset(predictions, targets, cont_indices, ent_indices):
+        all_indices = cont_indices + ent_indices
+        cont_acc = np.mean(predictions[cont_indices] == targets[cont_indices])
+        ent_acc = np.mean(predictions[ent_indices] == targets[ent_indices])
+        all_acc = np.mean(predictions[all_indices] == targets[all_indices])
+        return cont_acc, ent_acc, all_acc
+
+    print_strings = []
+    for path in path_list:
 
         print(f"Loading model from {path}.", file=sys.stderr)
         model = model_class.load_from_checkpoint(path, use_bias_probs=False)
         model.eval()
         device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         model.to(device)
-
-        # Load dataset
-        mnli: DatasetDict = datasets.load_dataset("glue", "mnli")
-        # Remove unnecessary splits for speed
-        mnli = DatasetDict(
-            {"validation_matched": mnli["validation_matched"], "validation_mismatched": mnli["validation_mismatched"]}
-        )
-        tokenizer = AutoTokenizer.from_pretrained(model.hparams.huggingface_model)
-
-        def preprocess_func(examples: dict):
-            args = (examples["premise"], examples["hypothesis"])
-            result = tokenizer(*args, max_length=max_length, truncation=True)
-            return result
-
-        mnli = mnli.map(preprocess_func, batched=True, num_proc=4)
-        mnli = mnli.rename_column("label", "labels")
-        mnli.set_format(
-            "torch",
-            columns=[
-                "input_ids",
-                "attention_mask",
-                "token_type_ids",
-                "labels",
-                "idx",
-            ],
-        )
-        # train_dataloader = DataLoader(
-        #     mnli["train"], batch_size=batch_size, shuffle=False, collate_fn=DataCollatorWithPadding(tokenizer=tokenizer)
-        # )
-        matched_dataloader = DataLoader(
-            mnli["validation_matched"],
-            batch_size=batch_size,
-            shuffle=False,
-            collate_fn=DataCollatorWithPadding(tokenizer=tokenizer),
-        )
-        mismatched_dataloader = DataLoader(
-            mnli["validation_mismatched"],
-            batch_size=batch_size,
-            shuffle=False,
-            collate_fn=DataCollatorWithPadding(tokenizer=tokenizer),
-        )
-
-        with open("data/subsets/mnli_negation_indices.json") as _file:
-            negation_indices = json.load(_file)
-        with open("data/subsets/mnli_lex_overlap_indices.json") as _file:
-            overlap_indices = json.load(_file)
-        with open("data/subsets/mnli_neg_ent_new_indices.json") as _file:
-            new_neg_ent_indices = json.load(_file)
-
-        def evaluate_subset(predictions, targets, cont_indices, ent_indices):
-            all_indices = cont_indices + ent_indices
-            cont_acc = np.mean(predictions[cont_indices] == targets[cont_indices])
-            ent_acc = np.mean(predictions[ent_indices] == targets[ent_indices])
-            all_acc = np.mean(predictions[all_indices] == targets[all_indices])
-            return cont_acc, ent_acc, all_acc
 
         results = []
         # Run matched evaluation
@@ -168,8 +175,9 @@ def evaluate_on_mnli(
                 )
                 results.extend([lex_cont_acc, lex_ent_acc, lex_acc])
 
-        print(",".join([str(result) for result in results]))
+        print_strings.append(",".join([str(result) for result in results]))
 
+    print("\n".join(print_strings))
 
 if __name__ == "__main__":
     fire.Fire(evaluate_on_mnli)
