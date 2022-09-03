dd23bb0570b3ceb61c630a8f52947d984eeaea36
Johannes Mario Meissner <jmariomeissner@gmail.com>
2022-09-03 10:07:42 +0900

    Script update

--- batch_extract.sh
+++ batch_extract.sh
@@ -2,9 +2,9 @@ for i in {0..4}
 do
     echo "Running i=$i"
     printnum=`expr $i + 1`
-    python extract_preds.py \
-    /remote/csifs1/disk0/meissner/shortcut-pruning/experiments/weak/undertrain/bert/mnli/multiruns/2022-04-18/16-09-54/$i/checkpoints/last.ckpt /
-    data/weak_models/undertrained/mnli/undertrain-mnli-1$i.json \
+    python scripts/extract_preds.py \
+    /PATH.../checkpoints/last.ckpt /
+    /PATH.../undertrain-mnli-1$i.json \
     --dataset_name mnli
 done
 echo "Finished batch script!"
